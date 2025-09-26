ec0e67cf0e7ef9106dc518809d6aa8fbfa8bbaee
gopinath87607 <85057872+gopinath87607@users.noreply.github.com>
2025-09-26 15:32:57 +0530

    Add files via upload

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import DeepResearchDashboard from './components/DeepResearchDashboard';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <DeepResearchDashboard />
+    </div>
+  );
+}
+
+export default App;
